d5e28dbd3ea565113fe6c5ab31cabc4d59e406a6
OpKris <kristofferopdahl@mail.com>
2020-11-22 13:55:20 +0100

    fixed line space, and removed variables names

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "static-site" {
-  project = var.project_id
+  project = "devops-eksamen-295808"
+//  project = var.project_id
   name = "devops-eksamen-bucket-v2"
   location = "EU"
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_run_service" "default" {
-  name     = "google-cloud-42opkris42-v1"
+  name = "google-cloud-42opkris42-v1"
   location = "us-central1"
   project = "devops-eksamen-295808"
 
@@ -8,11 +8,6 @@ resource "google_cloud_run_service" "default" {
     spec {
       containers {
         image = "gcr.io/devops-eksamen-295808/devops-eksamen:783eb71cde799cfa2128861c1a74b3f95c26e69c"
-        resources {
-          limits = {
-            memory: 512
-          }
-        }
         env {
           name = "LOGZ_TOKEN"
           value = var.logz_token
@@ -20,6 +15,11 @@ resource "google_cloud_run_service" "default" {
         env {
           name = "LOGZ_URL"
           value = var.logz_url
+        }
+        resources {
+          limits = {
+            memory: 512
+          }
         }
       }
     }
@@ -37,9 +37,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  project = google_cloud_run_service.default.project
+  service = google_cloud_run_service.default.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
